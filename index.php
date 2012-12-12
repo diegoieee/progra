d5b72bdd731f8e924833c5ea8a03d6a1912fbb42
Diego Pablo <diegoskate4@hotmail.com>
2012-12-12 11:13:21 -0600

    Se agrego la clase FrameSync

--- index.php
+++ index.php
@@ -1,16 +1,46 @@
-sdsdaddddddddsada
-sadasdsa
-das
-dsa
-d
-as
-s
-as
-a
-s
-sad
-sd
-asd
-ds
-as
-sdaaaaaaaaaaaaaaa
+#ifndef FRAMESYNC_H
+#define FRAMESYNC_H
+#include "stdafx.h"
+#include "base.h"
+/*!
+  *@class FrameSync
+  *@brief Clase.
+  */
+class FrameSync : public Base
+{
+    public:
+        FrameSync();
+        ~FrameSync();
+        int     Proccesing(void);
+        bool    GetAlarFlag(void);
+        bool    GetSincronyFlag(void);
+
+        int     Alignment(void);
+        bool    GetMFAlarmFlag(void);
+        bool    GetMFSyncronyFlag(void);
+
+    private:
+        void    SignalProccesing(int fm, int f, int c);
+        void    TransitionState ();
+        void    EvenState();
+        void    OddState();
+
+        char state;
+        unsigned int countByte; //Contador de byte
+        unsigned int countFrame;//Contador de trama
+        unsigned int countAlarm;//Contador de alarma
+
+        bool    mfAlarm;
+        bool    mfSync;
+        int     countMFsync;
+        int     countMFAlarm;
+
+        bool syncPatt;  //Señal de ocurrencia de patron de sincronía
+        bool evenPatt;  //Señal de ocurrencia de patrón par
+        bool falarm;     //Señal de alarma
+        bool fsynchrony; //Señal de sincronía
+        bool fsync;     //Banderas de apoyo
+        bool unavez;    //Bandera de apoyo
+};
+
+#endif // FRAMESYNC_H
